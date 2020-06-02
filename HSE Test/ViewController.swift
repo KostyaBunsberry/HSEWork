78cc4bc256cd3c831a01b8e2946db8cd1ad19282
Kostya Bunsberry <kostya.bunsberry@gmail.com>
2020-06-02 14:22:59 +0700

    Initial Commit

---
+++ HSE Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HSE Test
+//
+//  Created by Kostya Bunsberry on 02.06.2020.
+//  Copyright © 2020 Kostya Bunsberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
